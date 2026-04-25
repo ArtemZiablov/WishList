f105e5b48147c65593934a773902466f7d4aeb12
Artem Ziablov <artem.ziablov13@gmail.com>
2026-04-25 22:45:01 +0200

    Add FriendshipCreatedAt field to the data model

--- WishListApp/Models/Friendship.cs
+++ WishListApp/Models/Friendship.cs
@@ -6,7 +6,7 @@ public class Friendship
     public Guid RequesterId { get; set; }
     public Guid AddresseeId { get; set; }
     public FriendshipStatus Status { get; set; } = FriendshipStatus.Pending;
-
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public User Requester { get; set; } = null!;
     public User Addressee { get; set; } = null!;
 }
